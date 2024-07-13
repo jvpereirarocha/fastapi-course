430f41a78ff3c981b59f54fd4ffba8b391b0df0e
Joao Victor <jvpereirarocha@gmail.com>
2024-07-13 12:28:36 -0300

    finishing in-memory crud of users

--- fast_course/routers/users.py
+++ fast_course/routers/users.py
@@ -1,12 +1,17 @@
 from http import HTTPStatus
 
 from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
 
 from fast_course.schemas.users import UserDB, UserList, UserPublic, UserSchema
 
 user_router = APIRouter(prefix='/users')
 
-FAKE_DB = []
+FAKE_DB: list[UserDB] = []
+
+
+def invalid_id(id: int) -> bool:
+    return id > len(FAKE_DB) or id < 1
 
 
 @user_router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
@@ -20,3 +25,38 @@ def create_user(user_schema: UserSchema):
 @user_router.get('/', status_code=HTTPStatus.OK, response_model=UserList)
 def get_all_users():
     return {'users': FAKE_DB}
+
+
+@user_router.get(
+    '/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic | None
+)
+def fetch_one_user(user_id: int):
+    for user in FAKE_DB:
+        if user.id == user_id:
+            return user
+
+    return HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+
+@user_router.put(
+    '/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic | None
+)
+def update_user_by_id(user_id: int, user_schema: UserSchema):
+    if invalid_id(id=user_id):
+        return HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    user_with_id = UserDB(**user_schema.model_dump(), id=user_id)
+    FAKE_DB[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@user_router.delete(
+    '/{user_id}', status_code=HTTPStatus.NO_CONTENT, response_model=None
+)
+def delete_one_user(user_id: int):
+    if invalid_id(id=user_id):
+        return HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    del FAKE_DB[user_id - 1]
+    return None
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,10 @@
 from http import HTTPStatus
 
 
-def test_successfull_user_creation(client, mock_user_schema, mock_user_public):
+def test_user_creation(client, mock_user_schema, mock_user_public):
     user_data = {**mock_user_schema}
     response = client.post('/api/v1/users/', json=user_data)
-    assert response.status_code == HTTPStatus.CREATED  # 201 - CREATED
+    assert response.status_code == HTTPStatus.CREATED  # 201
     user_public = mock_user_public(user_schema=mock_user_schema)
     assert response.json() == {
         **user_public,
@@ -17,3 +17,30 @@ def test_get_all_users(client, mock_user_schema, mock_user_public):
     user_public = mock_user_public(mock_user_schema)
     user_public['id'] = 1
     assert response.json() == {'users': [user_public]}
+
+
+def test_fetch_one_user(client, mock_user_schema, mock_user_public):
+    response = client.get('/api/v1/users/1')
+    assert response.json() == {**mock_user_public(mock_user_schema), 'id': 1}
+
+
+def test_user_update(client, mock_user_schema, mock_user_public):
+    user_data = mock_user_schema
+    response = client.post('/api/v1/users/', json=user_data)
+    assert response.status_code == HTTPStatus.CREATED  # 201
+
+    # now, let's update the user
+    new_email = 'mynewemail@gmail.com'
+    user_data['email'] = new_email
+    response = client.put('/api/v1/users/1', json=user_data)
+    assert response.status_code == HTTPStatus.OK  # 200
+    assert response.json() == {
+        'username': user_data.get('username'),
+        'email': new_email,
+        'id': 1,
+    }
+
+
+def test_user_delete(client, mock_user_schema, mock_user_public):
+    response = client.delete('/api/v1/users/1')
+    assert response.status_code == HTTPStatus.NO_CONTENT  # 204
